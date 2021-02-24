e1745ccbec468ba4915c85a1eee37a2ef2e27320
FERNANDO FURTADO <fernando.furtadocarrilho@gmail.com>
2021-02-24 08:21:46 -0300

    Adicionando READMEem Java-2020

---
+++ java-2020/README.cmd
@@ -0,0 +1,12 @@
+# Learned Topics
+
+ 1. Oject Oriented Programming (OOP):
+  1.1 Encapsulation;
+  1.2 Heritage;
+  1.3 Polymorphism.
+
+ 2. Anonymous Classes
+
+ 3. Anonymous Objects
+
+ 4. Error Handling
